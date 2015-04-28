a401ccfb2dbbce25fca5c8b73930e204d8c552f0
mentalwall <chriswright757@gmail.com>
2015-04-28 16:15:40 +0100

    API Link and Mag Read

---
+++ CrossBeam/CrossBeam/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CrossBeam
+{
+    partial class APITest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.APIInitialise = new System.Windows.Forms.Button();
+            this.GetMag = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // APIInitialise
+            // 
+            this.APIInitialise.Location = new System.Drawing.Point(67, 43);
+            this.APIInitialise.Name = "APIInitialise";
+            this.APIInitialise.Size = new System.Drawing.Size(75, 23);
+            this.APIInitialise.TabIndex = 0;
+            this.APIInitialise.Text = "API ";
+            this.APIInitialise.UseVisualStyleBackColor = true;
+            this.APIInitialise.Click += new System.EventHandler(this.APIInitialise_Click);
+            // 
+            // GetMag
+            // 
+            this.GetMag.Location = new System.Drawing.Point(269, 42);
+            this.GetMag.Name = "GetMag";
+            this.GetMag.Size = new System.Drawing.Size(75, 23);
+            this.GetMag.TabIndex = 1;
+            this.GetMag.Text = "Read Mag";
+            this.GetMag.UseVisualStyleBackColor = true;
+            this.GetMag.Click += new System.EventHandler(this.GetMag_Click_1);
+            // 
+            // APITest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 643);
+            this.Controls.Add(this.GetMag);
+            this.Controls.Add(this.APIInitialise);
+            this.MinimizeBox = false;
+            this.Name = "APITest";
+            this.Text = "API Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button APIInitialise;
+        private System.Windows.Forms.Button GetMag;
+    }
+}
+
---
+++ CrossBeam/CrossBeam/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using APILib;
+
+namespace CrossBeam
+{
+    public partial class APITest : Form
+    {
+        Api CZEMApi = new Api();
+        public APITest()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void APIInitialise_Click(object sender, EventArgs e)
+        {
+
+
+            // Initialise the control to connect to the local CZ EM microscope
+            long lReturn = CZEMApi.InitialiseRemoting();
+
+            if (lReturn != 0)
+            {
+                MessageBox.Show("Failed to initialise control");
+                return;
+            }
+        }
+
+        private void GetMag_Click_1(object sender, EventArgs e)
+        {
+            // Get the magnification value from the microscope
+            object varfloat = new System.Runtime.InteropServices.VariantWrapper((float)0.0f);
+            // get param (numeric)value
+            long lResult = CZEMApi.Get("AP_MAG", ref varfloat);
+            if (lResult != 0)
+            {
+                MessageBox.Show("Failed to get magnification");
+                return;
+            }
+            MessageBox.Show("Magnification = " + varfloat.ToString());
+        }      
+
+    }
+}
